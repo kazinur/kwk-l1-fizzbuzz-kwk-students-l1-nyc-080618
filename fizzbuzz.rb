59945531ef5ff54040abbea3c772e43ff8cc30c0
kazinur <kazinur@mybihs.org>
2018-08-07 18:37:27 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+number= 15 
+def FizzBuzz (number)
+  if number % 15 ==0
+    return "FizzBuzz"
+    elsif number % 3==0
+      return "Fizz"
+    elsif number % 5==0
+      return "Buzz"
+    end
+end
+ 
